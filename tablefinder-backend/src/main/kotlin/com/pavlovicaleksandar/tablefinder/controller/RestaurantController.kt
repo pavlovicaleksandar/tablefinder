80ac7bbb797fc9069abc995671c272c3dda9be4f
Aleksandar Pavlovic <pavlovic@bryter.io>
2023-05-14 19:14:09 +0200

    Introduce restaurants table, repository, service and implement getAll

--- tablefinder-backend/src/main/kotlin/com/pavlovicaleksandar/tablefinder/controller/RestaurantController.kt
+++ tablefinder-backend/src/main/kotlin/com/pavlovicaleksandar/tablefinder/controller/RestaurantController.kt
@@ -1,18 +1,40 @@
 package com.pavlovicaleksandar.tablefinder.controller
 
+import com.pavlovicaleksandar.tablefinder.service.Restaurant
+import com.pavlovicaleksandar.tablefinder.service.RestaurantService
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
-data class Restaurant(val name: String, val description: String)
+import java.util.UUID
 
 @RestController
 @RequestMapping("/restaurants")
 @CrossOrigin(origins = ["http://localhost:3000"])
-class RestaurantController() {
+class RestaurantController(private val service: RestaurantService) {
     @GetMapping
-    fun getAllRestaurants(): List<Restaurant> {
-        return listOf(Restaurant(name = "Restaurant 1", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."))
+    fun getAllRestaurants(): List<RestaurantResponseDTO> {
+        return service.findAll().toRestaurantResponseDTO()
     }
+
+//    @GetMapping("/{restaurantId}")
+//    fun getRestaurantById(restaurantId: UUID): RestaurantResponseDTO {
+//        return RestaurantResponseDTO(name = "Restaurant 1", description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+//    }
+}
+
+data class RestaurantResponseDTO(val id: UUID, val name: String, val description: String)
+
+private fun List<Restaurant>.toRestaurantResponseDTO(): List<RestaurantResponseDTO> {
+    return map {
+        it.toRestaurantResponseDTO()
+    }
+}
+
+private fun Restaurant.toRestaurantResponseDTO(): RestaurantResponseDTO {
+    return RestaurantResponseDTO(
+        id = this.id,
+        name = this.name,
+        description = this.description
+    )
 }
