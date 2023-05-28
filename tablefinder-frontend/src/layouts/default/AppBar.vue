3d3ef4672c03803d84629bf0f6c937539b318b7d
Aleksandar Pavlovic <pavlovic@bryter.io>
2023-05-28 17:02:18 +0200

    Modify app bar

--- tablefinder-frontend/src/layouts/default/AppBar.vue
+++ tablefinder-frontend/src/layouts/default/AppBar.vue
@@ -4,18 +4,18 @@
 <!--      <v-icon icon="mdi-circle-slice-6" />-->
       <div
         class="text-no-wrap bg-secondary"
-        style="width: 3.6rem; font-size: x-large"
+        style="width: 4.8rem; font-size: xx-large"
       >
       TableFinder
       </div>
-      <div>
-        <router-link to="/restaurants"><v-btn>Restaurants</v-btn></router-link>
-        <router-link to="/reservations"><v-btn>Reservations</v-btn></router-link>
-        <router-link to="/users"><v-btn>Users</v-btn></router-link>
-        <router-link to="/registration"><v-btn>Register</v-btn></router-link>
-        <router-link to="/"><v-btn>Login</v-btn></router-link>
-      </div>
     </v-app-bar-title>
+    <div>
+      <router-link to="/restaurants"><v-btn>Restaurants</v-btn></router-link>
+      <router-link to="/reservations"><v-btn>Reservations</v-btn></router-link>
+      <router-link to="/users"><v-btn>Users</v-btn></router-link>
+      <router-link to="/registration"><v-btn>Register</v-btn></router-link>
+      <router-link to="/"><v-btn>Login</v-btn></router-link>
+    </div>
     <div>
 
     </div>
