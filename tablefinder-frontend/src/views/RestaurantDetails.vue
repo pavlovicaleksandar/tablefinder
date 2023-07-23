1702b767d6fb3e24a61eb107a90ad49525183ab6
Aleksandar Pavlovic <pavlovic@bryter.io>
2023-07-23 15:09:32 +0200

    Change how reviews are positionated

--- tablefinder-frontend/src/views/RestaurantDetails.vue
+++ tablefinder-frontend/src/views/RestaurantDetails.vue
@@ -25,103 +25,86 @@
           </v-col>
         </v-row>
       </v-card-text>
-    </v-card>
-<!--    <div class="wrapper">-->
-<!--      <div class="d-flex gap-12">-->
-<!--        <v-img :src='restaurant.imageUrl' width="50%"></v-img>-->
-<!--        <div class="restaurant-description">-->
-<!--          <span class="text-h4">{{ restaurant.name }}</span>-->
-<!--          <div>-->
-<!--            <v-chip v-for="tag in restaurant.tags" :key="tag.id">{{tag.tagName}}</v-chip>-->
-<!--          </div>-->
-<!--          <v-rating readonly v-model="restaurant.rating" color="secondary" half-increments></v-rating>-->
-<!--          <span>Number of reviews: ({{restaurant.numberOfRatings}})</span>-->
-<!--          <h3>Price: {{['N/A', '$', '$$', '$$$'].at(restaurant.price)}}</h3>-->
-<!--          <span>{{ restaurant.description }}</span>-->
-<!--          <div class="reserve-now-btn">-->
-<!--            <ReservePopup></ReservePopup>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-    <v-card>
       <v-card-title class="headline">User Reviews</v-card-title>
       <v-card-text>
-        <v-list>
-          <v-list-item v-if="isAddReviewEnabled === true">
-            <v-card>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" md="12">
-                      <v-rating
-                        v-model="selectedRating"
-                        bg-color="orange-lighten-1"
-                        color="primary"
-                      ></v-rating>
-                    </v-col>
-                    <v-col cols="12" md="12">
-                      <v-select color="primary"
-                                v-model="selectedOptionForPrice"
-                                label="Price"
-                                variant="outlined"
-                                :items="priceOptions"
-                                item-value="value"
-                                item-title="displayText"
-                                return-object
-                                single-line
-                                >
-                      </v-select>
-                    </v-col>
-                    <v-col cols="12" md="12">
-                      <v-textarea color="primary" v-model="comment"  label="Review (optional)" variant="outlined"></v-textarea>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn v-if="selectedRating!=null" color="primary" @click="addReview" variant="elevated" class="mt-n8" style="min-width: 120px;">Add</v-btn>
-                <v-btn v-if="selectedRating == null" color="primary" @click="addReview" variant="elevated" disabled class="mt-n8" style="min-width: 120px;">Add</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-list-item>
-          <v-list-item v-for="review in reviews" :key="review.id">
-            <v-card>
-              <v-card-title>{{ review.username }}</v-card-title>
-              <v-card-subtitle>
-                {{ new Date(review.createdAt).toLocaleString() }}
-              </v-card-subtitle>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" md="12">
-                      <h3> Price: {{['$', '$$', '$$$'].at(review.price - 1)}}</h3>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col cols="12" md="12">
-                      <v-rating
-                        v-model="review.rating"
-                        bg-color="orange-lighten-1"
-                        color="primary"
-                        readonly
-                      ></v-rating>
-                    </v-col>
-                  </v-row>
-                  <v-row>
-                    <v-col cols="12" md="12">
-                      <p>{{ review.comment }}</p>
-                    </v-col>
-                  </v-row>
-                  <v-row justify="end">
-                    <v-btn v-if="user.role === 'Admin' || user.username === review.username" color="red" @click="removeReview(review.id)" style="min-width: 120px;">Remove</v-btn>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-            </v-card>
-            <hr>
-          </v-list-item>
-        </v-list>
+        <v-card>
+          <v-card-text>
+            <v-list>
+              <v-list-item v-if="isAddReviewEnabled === true">
+                <v-card>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" md="12">
+                          <v-rating
+                            v-model="selectedRating"
+                            bg-color="orange-lighten-1"
+                            color="primary"
+                          ></v-rating>
+                        </v-col>
+                        <v-col cols="12" md="12">
+                          <v-select color="primary"
+                                    v-model="selectedOptionForPrice"
+                                    label="Price"
+                                    variant="outlined"
+                                    :items="priceOptions"
+                                    item-value="value"
+                                    item-title="displayText"
+                                    return-object
+                                    single-line
+                          >
+                          </v-select>
+                        </v-col>
+                        <v-col cols="12" md="12">
+                          <v-textarea color="primary" v-model="comment"  label="Review (optional)" variant="outlined"></v-textarea>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn v-if="selectedRating!=null" color="primary" @click="addReview" variant="elevated" class="mt-n8" style="min-width: 120px;">Add</v-btn>
+                    <v-btn v-if="selectedRating == null" color="primary" @click="addReview" variant="elevated" disabled class="mt-n8" style="min-width: 120px;">Add</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-list-item>
+              <v-list-item v-for="review in reviews" :key="review.id">
+                <v-card>
+                  <v-card-title>{{ review.username }}</v-card-title>
+                  <v-card-subtitle>
+                    {{ new Date(review.createdAt).toLocaleString() }}
+                  </v-card-subtitle>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" md="12">
+                          <h3> Price: {{['$', '$$', '$$$'].at(review.price - 1)}}</h3>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" md="12">
+                          <v-rating
+                            v-model="review.rating"
+                            bg-color="orange-lighten-1"
+                            color="primary"
+                            readonly
+                          ></v-rating>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="12" md="12">
+                          <p>{{ review.comment }}</p>
+                        </v-col>
+                      </v-row>
+                      <v-row justify="end">
+                        <v-btn v-if="user.role === 'Admin' || user.username === review.username" color="red" @click="removeReview(review.id)" style="min-width: 120px;">Remove</v-btn>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                </v-card>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+        </v-card>
       </v-card-text>
     </v-card>
   </v-container>
